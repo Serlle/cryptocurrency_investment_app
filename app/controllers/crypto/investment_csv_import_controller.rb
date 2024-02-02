e2fbe74db50432545ded4bf8083b439b4a3ab9fc
Serlle Rosale <serlle@caraji.io>
2024-02-02 15:27:38 -0600

    changed variable

--- app/controllers/crypto/investment_csv_import_controller.rb
+++ app/controllers/crypto/investment_csv_import_controller.rb
@@ -6,8 +6,8 @@ class Crypto::InvestmentCsvImportController < ApplicationController
     uploaded_file = csv_file_params[:csv_file]
     if uploaded_file.present? && uploaded_file.content_type == "text/csv"
       csv_data = CsvReaderService.new(uploaded_file.path).read
-      calculator = InvestmentCalculatorService.new(csv_data)
-      calculated_value = calculator.calculate
+      investment = InvestmentCalculatorService.new(csv_data)
+      calculated_value = investment.calculate
       flash[:success] = 'Archivo CSV importado y calculo hecho exitosamente.'
     else
       flash[:error] = 'Por favor, selecciona un archivo CSV válido.'
